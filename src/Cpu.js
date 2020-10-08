5d5448accc765c41865afb64ab3ab09b25d3da0c
theapi <petermclarke@gmail.com>
2020-10-08 10:54:42 +0100

    Registers

--- src/Cpu.js
+++ src/Cpu.js
@@ -7,7 +7,16 @@ class Cpu extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      clk: false,
+      clk: false,       // Cpu clock
+      pc: 0,            // Program counter
+      uc: 0,            // Micro code counter
+      bus: 0,           // The main bus
+      a_reg: 0,         // A register (accumulator)
+      b_reg: 0,         // B register
+      out_reg: 0,       // Output register, what is shown on the output display
+      mar: 0,           // Memory address register
+      instruction: 0,   // Instruction register
+      ctl_word: 0,      // The control word
     };
   }
 
