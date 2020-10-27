510a3afee9a462111e53e90861a43c94d82426b3
theapi <petermclarke@gmail.com>
2020-10-27 19:14:21 +0000

    Waveform order

--- src/components/Visual.js
+++ src/components/Visual.js
@@ -48,38 +48,43 @@ const Visual = () => {
 
         <Canvas width={width} height={height} className="canvas" clk={clk}
           name="Clock" waveform={waves['clk']} signal={clk} />
+
+        {/* Fetch */}
         <Canvas width={width} height={height} className="canvas" clk={clk}
           name="Counter Out" waveform={waves['co']} signal={controlWord.co} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
+          name="MAR in" waveform={waves['mi']} signal={controlWord.mi} />
+        <Canvas width={width} height={height} className="canvas" clk={clk}
+          name="RAM out" waveform={waves['ro']} signal={controlWord.ro} />
+        <Canvas width={width} height={height} className="canvas" clk={clk}
+          name="Instruction in" waveform={waves['ii']} signal={controlWord.ii} />
+        <Canvas width={width} height={height} className="canvas" clk={clk}
           name="Counter Enable" waveform={waves['ce']} signal={controlWord.ce} />
+        {/* end fetch */}
+
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="Output register in" waveform={waves['oi']} signal={controlWord.oi} />
-        <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="B register in" waveform={waves['bi']} signal={controlWord.bi} />
+          name="Instruction out" waveform={waves['io']} signal={controlWord.io} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
           name="Subtract" waveform={waves['su']} signal={controlWord.su} />
+        <Canvas width={width} height={height} className="canvas" clk={clk}
+          name="A register in" waveform={waves['ai']} signal={controlWord.ai} />
+        <Canvas width={width} height={height} className="canvas" clk={clk}
+          name="B register in" waveform={waves['bi']} signal={controlWord.bi} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
           name="ALU out" waveform={waves['eo']} signal={controlWord.eo} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
           name="A register out" waveform={waves['ao']} signal={controlWord.ao} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="A register in" waveform={waves['ai']} signal={controlWord.ai} />
-        <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="Instruction in" waveform={waves['ii']} signal={controlWord.ii} />
-        <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="Instruction out" waveform={waves['io']} signal={controlWord.io} />
-        <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="RAM out" waveform={waves['ro']} signal={controlWord.ro} />
+          name="Output register in" waveform={waves['oi']} signal={controlWord.oi} />
+
         <Canvas width={width} height={height} className="canvas" clk={clk}
           name="RAM in" waveform={waves['ri']} signal={controlWord.ri} />
-        <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="MAR in" waveform={waves['mi']} signal={controlWord.mi} />
-        <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="Halt" waveform={waves['hlt']} signal={controlWord.hlt} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
           name="Jump" waveform={waves['j']} signal={controlWord.j} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
           name="Jump conditional" waveform={waves['jc']} signal={controlWord.jc} />
+        <Canvas width={width} height={height} className="canvas" clk={clk}
+          name="Halt" waveform={waves['hlt']} signal={controlWord.hlt} />
 
       </div>
     </div>
