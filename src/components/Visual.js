9605be0a48e0590551d4adbb6d793cffb0830eb4
Peter Clarke <petermclarke@gmail.com>
2020-10-28 10:29:36 +0000

    Control word label in the waveform label

--- src/components/Visual.js
+++ src/components/Visual.js
@@ -40,10 +40,6 @@ const Visual = () => {
   return (
     <div id="visual">
 
-      <h6>A visual representation of what's happening inside the cpu goes here</h6>
-
-      <Led on={clk}  />
-      <Controller />
       <div className="logicAnalyser">
 
         <Canvas width={width} height={height} className="canvas" clk={clk}
@@ -51,40 +47,40 @@ const Visual = () => {
 
         {/* Fetch */}
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="Counter Out" waveform={waves['co']} signal={controlWord.co} />
+          name={`Counter out [co ${controlWord.co}]`} waveform={waves['co']} signal={controlWord.co} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="MAR in" waveform={waves['mi']} signal={controlWord.mi} />
+          name={`MAR in [mi ${controlWord.mi}]`} waveform={waves['mi']} signal={controlWord.mi} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="RAM out" waveform={waves['ro']} signal={controlWord.ro} />
+          name={`RAM out [co ${controlWord.ro}]`} waveform={waves['ro']} signal={controlWord.ro} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="Instruction in" waveform={waves['ii']} signal={controlWord.ii} />
+          name={`Instruction in [ii ${controlWord.ii}]`} waveform={waves['ii']} signal={controlWord.ii} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="Counter Enable" waveform={waves['ce']} signal={controlWord.ce} />
+          name={`Counter enable [ce ${controlWord.ce}]`} waveform={waves['ce']} signal={controlWord.ce} />
         {/* end fetch */}
 
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="Instruction out" waveform={waves['io']} signal={controlWord.io} />
+          name={`Instruction out [io ${controlWord.io}]`} waveform={waves['io']} signal={controlWord.io} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="Subtract" waveform={waves['su']} signal={controlWord.su} />
+          name={`Subtract [su ${controlWord.su}]`} waveform={waves['su']} signal={controlWord.su} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="A register in" waveform={waves['ai']} signal={controlWord.ai} />
+          name={`A register in [ai ${controlWord.ai}]`} waveform={waves['ai']} signal={controlWord.ai} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="B register in" waveform={waves['bi']} signal={controlWord.bi} />
+          name={`B register in [bi ${controlWord.bi}]`} waveform={waves['bi']} signal={controlWord.bi} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="ALU out" waveform={waves['eo']} signal={controlWord.eo} />
+          name={`ALU out [eo ${controlWord.eo}]`} waveform={waves['eo']} signal={controlWord.eo} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="A register out" waveform={waves['ao']} signal={controlWord.ao} />
+          name={`A register out [ao ${controlWord.ao}]`} waveform={waves['ao']} signal={controlWord.ao} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="Output register in" waveform={waves['oi']} signal={controlWord.oi} />
+          name={`Output register in [oi ${controlWord.oi}]`} waveform={waves['oi']} signal={controlWord.oi} />
 
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="RAM in" waveform={waves['ri']} signal={controlWord.ri} />
+          name={`RAM in [ri ${controlWord.ri}]`} waveform={waves['ri']} signal={controlWord.ri} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="Jump" waveform={waves['j']} signal={controlWord.j} />
+          name={`Jump [j ${controlWord.j}]`} waveform={waves['j']} signal={controlWord.j} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="Jump conditional" waveform={waves['jc']} signal={controlWord.jc} />
+          name={`Jump conditional [jc ${controlWord.jc}]`} waveform={waves['jc']} signal={controlWord.jc} />
         <Canvas width={width} height={height} className="canvas" clk={clk}
-          name="Halt" waveform={waves['hlt']} signal={controlWord.hlt} />
+          name={`Halt [hlt ${controlWord.hlt}]`} waveform={waves['hlt']} signal={controlWord.hlt} />
 
       </div>
     </div>
