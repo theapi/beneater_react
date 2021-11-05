a4de129c251db53d88abad654612d0a8bbf36bd6
theapi <petermclarke@gmail.com>
2021-11-05 11:14:20 +0000

    Host on github

---
+++ build/precache-manifest.10414e61fbea1e808101042e5d335fd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f65dbb9d459630afc3faef9162437d73",
+    "url": "/beneater_react/index.html"
+  },
+  {
+    "revision": "b91d8d35fe7dee18d8d2",
+    "url": "/beneater_react/static/css/main.9cb73f7d.chunk.css"
+  },
+  {
+    "revision": "5f1707b85255262507b1",
+    "url": "/beneater_react/static/js/2.5c3426ba.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/beneater_react/static/js/2.5c3426ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b91d8d35fe7dee18d8d2",
+    "url": "/beneater_react/static/js/main.11f2dfac.chunk.js"
+  },
+  {
+    "revision": "687789cc16ce4e0d64d9",
+    "url": "/beneater_react/static/js/runtime-main.f7848a15.js"
+  }
+]);
